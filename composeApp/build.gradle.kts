aa39664beff2b851a4309a6b762a0a502b5f20b0
Azizjon <azizconi1@gmail.com>
2026-08-03 16:48:38 +0500

    prepare android release 1.0.1

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -106,8 +106,8 @@ android {
         applicationId = "tj.app.quran_todo"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "1.0.0"
+        versionCode = 6
+        versionName = "1.0.1"
     }
     packaging {
         resources {
